bb3c348a9eef389ebecbc2caeaea384abe935090
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-28 23:26:40 +0200

    Update owner-addprem.js

--- plugins/owner-addprem.js
+++ plugins/owner-addprem.js
@@ -5,23 +5,23 @@ let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
     let user = global.db.data.users[who]
-    if (!who) throw `✳️ Tag or mention someone\n\n📌 Example : ${usedPrefix + command} @user`
-if (global.prems.includes(who.split`@`[0])) throw '✳️ The user Mentioned Already is premium'
+    if (!who) throw `✳️ قم بعمل علامة (@) أو منشن على شخص\n\n📌 مثال : ${usedPrefix + command} @user`
+if (global.prems.includes(who.split`@`[0])) throw '✳️ الشخص المذكور أصبح بالفعل مميز'
 global.prems.push(`${who.split`@`[0]}`)
 
 conn.reply(m.chat, `
-✅ PREMIUM
+✅ المميزين
 
-@${who.split`@`[0]} now you become a premium user
+@${who.split`@`[0]} الآن أصبحت مستخدمًا متميزًا
 ┌───────────
-▢ *Number:* ${user.name}
+▢ *الرقم:* ${user.name}
 └───────────
 `, m, { mentions: [who] })
 
 }
 handler.help = ['addprem <@tag>']
 handler.tags = ['owner']
-handler.command = ['addprem', 'addpremium'] 
+handler.command = ['تميز', 'بريم'] 
 
 handler.group = true
 handler.rowner = true
