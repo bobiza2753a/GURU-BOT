a899b702dfb10c198165842a4ec9cae9b45a97a9
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-28 23:31:17 +0200

    Update owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -3,11 +3,11 @@
 let handler = async (m, { conn, isOwner, isAdmin, isROwner }) => {
     if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
-    m.reply('✅ The bot was deactivated in this group')
+    m.reply('*[❗تنبيه❗] تم حظر هذه المجموعة من استخدام البوت*\n\n*—◉ لا يمكن لأعضاء المجموعة استخدام أوامر البوت بعد الآن*')
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
-handler.command = ['banchat', 'chatoff'] 
+handler.command = ['حظر', 'chatoff'] 
 
 export default handler
  
