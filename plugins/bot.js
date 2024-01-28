37cb0489b26e7207f59ad86e9882925d7af1d49e
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-28 23:20:18 +0200

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -1,19 +1,13 @@
+let handler = async (m, { conn }) => {
+    let user = global.db.data.users[m.sender];
+    let name = conn.getName(m.sender);
+    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
+    let message = `*هاا مـاذا تريـد/ي* ❓`;
 
-let handler = async (m, { conn}) => {
-let user = global.db.data.users[m.sender]
-let name = conn.getName(m.sender)
-let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let av = `./Assets/mp3/${pickRandom(["Guru", "Guru1", "Guru2", "Guru3", "Guru4"])}.mp3`
+    conn.sendFile(m.chat, 'https://telegra.ph/file/c06dce68aa5c883e358b1.jpg', 'image.jpg', message, m);
+};
 
-m.reply( `مرحبًا ${taguser} هل تحتاج إلى مساعدة؟ اكتب / الاوامر `)
-conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-} 
+handler.customPrefix = /^(|bot|بوت)$/i;
+handler.command = new RegExp;
 
-handler.customPrefix = /^(bot|Zoro|بوت)$/i
-handler.command = new RegExp
-
-export default handler
-
-function pickRandom(list) {
-  return list[Math.floor(list.length * Math.random())]
-}
+export default handler;
