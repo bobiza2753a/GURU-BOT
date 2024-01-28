46cf9a871eb52cc2caad91c9c7f51e2c96ef607c
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-28 19:07:20 +0200

    Update enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -243,7 +243,7 @@ m.reply(`
 }
 handler.help = ['en', 'dis'].map(v => v + 'able <option>')
 handler.tags = ['config']
-handler.command = /^((en|dis)able|(turn)?ت(n|عطيل)|[01])$/i
+handler.command = /^((ت|dis)شغيل|(turn)?ت(n|عطيل)|[01])$/i
 
 export default handler
 
