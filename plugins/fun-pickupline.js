d2c89fd11c7e9f9a8d36ea3a122197ad4838b579
YosefZoro1 <151135574+YosefZoro1@users.noreply.github.com>
2024-01-28 20:40:05 +0200

    Update fun-pickupline.js

--- plugins/fun-pickupline.js
+++ plugins/fun-pickupline.js
@@ -1,32 +1,21 @@
-import fetch from 'node-fetch';
-import translate from '@vitalets/google-translate-api';
+import fetch from 'node-fetch'
+import { translate } from '@vitalets/google-translate-api';
+let handler  = async (m, { conn }) => {
+  let res = await fetch(`https://api.popcat.xyz/pickuplines`)
+  if (!res.ok) throw await res.text()
+	    let json = await res.json()
 
-let yoMamaJokeHandler = async (m, { conn, text }) => {
-  try {
-    let res = await fetch(`https://api.popcat.xyz/pickuplines`);
+  let translatedText = await translate(json.pickupline, { to: 'ar' });
 
-    if (!res.ok) {
-      throw new Error(`فشل طلب API مع الحالة ${res.status}`);
-    }
-
-    let json = await res.json();
-
-    console.log('JSON response:', json);
-
-    let yoMamaJoke = `${json.pickupline}`;
-    
-    let translation = await translate(yoMamaJoke, { to: 'ar' });
-
-    let translatedYoMamaJoke = translation.text;
-
-    m.reply(translatedYoMamaJoke);
-  } catch (error) {
-    console.error(error);
-  }
+  conn.sendMessage(m.chat, { text: translatedText.text, mentions: [m.sender] }, { quoted: m });
 };
+handler.help = ['pickupline']
+handler.tags = ['pickupline']
+handler.command = /^(هبد)$/i
 
-handler.help = ['هبد'];
-handler.tags = ['fun'];
-handler.command = /^(هبد)$/i;
+export default handler
 
-export default handler;
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
